43a8d70b84e5da6b098f4420d81cd9243564cd0a
Lear <GitHub>
2026-03-17 20:33:02 +0800

    Fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Taiwan-Lottery-Simulator/',
+  base: '/Taiwan_Lottery_Simulator/',
 })
